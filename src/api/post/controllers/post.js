b3019bdff0286e3614cf4ba96ad2685da66871d8
leslie <2483677516@qq.com>
2023-02-10 17:46:15 +0800

    <feat>(api response)

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,4 +1,5 @@
 'use strict';
+const { removeAttrs } = require("../../../utils/index.js");
 
 /**
  * post controller
@@ -6,15 +7,30 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+// module.exports = createCoreController('api::post.post');
 
-// module.exports = createCoreController('api::post.post', ({ strapi }) => ({
-//     async find(ctx) {
-//         ctx.query = {
-//             ...ctx.query,
-//             populate: 'deep'
-//         }
-//         const { data } = await super.find(ctx)
-//         return removeAttrsAndId(removeTime(data[0]))
-//     },
-// }));
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            // populate: "deep",
+            populate: "*",
+        };
+        const { pageNo, pageSize, ...params } = ctx.query;
+        if (pageNo && pageSize) {
+          ctx.query = {
+            ...params,
+            "pagination[page]": Number(pageNo),
+            "pagination[pageSize]": Number(pageSize),
+          };
+        }
+        const res = await super.find(ctx);
+        res.data = res.data.map(item=>removeAttrs(item))
+        return res;
+    },
+    
+    async findOne(ctx) {
+        const { data } = await super.findOne(ctx);
+        return removeAttrs(data);
+    },
+}));
