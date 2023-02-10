95675924cac61c05597687d17252781ff3aec534
leslie <2483677516@qq.com>
2023-02-10 14:13:05 +0800

    <cache>(取消忽略缓存)

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -7,3 +7,14 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::post.post');
+
+// module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+//     async find(ctx) {
+//         ctx.query = {
+//             ...ctx.query,
+//             populate: 'deep'
+//         }
+//         const { data } = await super.find(ctx)
+//         return removeAttrsAndId(removeTime(data[0]))
+//     },
+// }));
